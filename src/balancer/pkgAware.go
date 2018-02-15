7f9efa422e7df57310d1a2e1f90878f594e87c9a
Gustavo Totoy <gtotoy@gmail.com>
2018-02-15 09:58:28 -0500

    Fix threshold condition in pkgAware.go. Move logging workers info from pkgAware.go to main.go. Change GET to POST request in asyncrequests.go

--- src/balancer/pkgAware.go
+++ src/balancer/pkgAware.go
@@ -1,10 +1,10 @@
 package balancer
 
 import (
-	"../schutil"
-	"hash/fnv"
 	"errors"
-	"log"
+	"hash/fnv"
+
+	"../schutil"
 )
 
 func h1(s string) uint32 {
@@ -19,9 +19,9 @@ func h2(s string) uint32 {
 	return hf.Sum32()
 }
 
-func SelectWorkerPkgAware(workers []schutil.Worker, 
-							pkgs []string, 
-							threshold int) (*schutil.Worker, error) {
+func SelectWorkerPkgAware(workers []schutil.Worker,
+	pkgs []string,
+	threshold int) (*schutil.Worker, error) {
 	if len(workers) == 0 {
 		return nil, errors.New("Can't select worker, Workers empty")
 	}
@@ -30,20 +30,16 @@ func SelectWorkerPkgAware(workers []schutil.Worker,
 		return nil, errors.New("Can't select worker, No largest package, pkgs empty")
 	}
 
-	for i, _ := range workers {
-		log.Printf("--> worker: %s with load: %d", workers[i].URL.String(), workers[i].Load)
-	}
-
 	largestPkg := pkgs[0]
-	targetIndex1 := h1(largestPkg)%uint32(len(workers))
-	targetIndex2 := h2(largestPkg)%uint32(len(workers))
+	targetIndex1 := h1(largestPkg) % uint32(len(workers))
+	targetIndex2 := h2(largestPkg) % uint32(len(workers))
 
 	targetIndex := targetIndex2
 	if workers[targetIndex1].Load < workers[targetIndex2].Load {
 		targetIndex = targetIndex1
 	}
-	
-	if workers[targetIndex].Load > threshold { // Find least loaded
+
+	if workers[targetIndex].Load >= threshold { // Find least loaded
 		targetIndex = 0
 		for i := 1; i < len(workers); i++ {
 			if workers[i].Load < workers[targetIndex].Load {
--- test/async requests/asyncrequests.go
+++ test/async requests/asyncrequests.go
@@ -1,37 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-func MakeRequest(url string, ch chan<- string) {
+func MakeRequest(url string, body []byte, ch chan<- string) {
 	start := time.Now()
-	resp, _ := http.Get(url)
+	resp, _ := http.Post(url, "application/json", bytes.NewBuffer(body))
 
 	secs := time.Since(start).Seconds()
-	body, _ := ioutil.ReadAll(resp.Body)
-	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, len(body), url)
+	b, _ := ioutil.ReadAll(resp.Body)
+	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, len(b), url)
 }
 
 func main() {
 	start := time.Now()
 	ch := make(chan string)
 	port := 9090
+	lambda := "hello"
+	body := []byte(`{"pkgs": ["fmt", "rand"], "name": "Moon"}`)
 	requests := []string{
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
-		fmt.Sprintf("http://localhost:%d/status", port),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
+		fmt.Sprintf("http://localhost:%d/runLambda/%s", port, lambda),
 	}
+	for range [2]struct{}{} {
+		requests = append(requests, requests...)
+	}
+
 	for _, url := range requests {
-		go MakeRequest(url, ch)
+		go MakeRequest(url, body, ch)
+		time.Sleep(500 * time.Microsecond)
 	}
 
 	for range requests { // Awaiting all responses
